669cbcf30bb8c9180a7891911aa353fce51aa2c6
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2022-04-05 23:14:17 +0300

    String from literal, char[], byte[] charset

--- HomeworkString/src/by/epam/traning/string/StringApp.java
+++ HomeworkString/src/by/epam/traning/string/StringApp.java
@@ -1,8 +1,28 @@
 package by.epam.traning.string;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
 public class StringApp {
 
     public static void main(String[] args) {
+        char[] charArray = {'s', 't', 'r', 'i', 'n', 'g'};
+        String literalString = "This is String from literal, дальше кириллица";
+        String constructorString = new String("String from constructor");
+        System.out.println(constructorString);
+        byte[] byteArrayDefaultEncoding = literalString.getBytes();
+        byte[] byteArrayCp1251Encoding = null;
+        try {
+            byteArrayCp1251Encoding = literalString.getBytes("cp1251");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        System.out.println(String.format("equals byte arrays: %b",
+            Arrays.equals(byteArrayDefaultEncoding, byteArrayCp1251Encoding)));
+        String stringFromCharArray = new String(charArray);
+        System.out.println(stringFromCharArray);
+        String stringFromByteArray = new String(byteArrayDefaultEncoding);
+        System.out.println(stringFromByteArray);
         System.out.println("Done ...");
     }
 }
