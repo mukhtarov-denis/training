07967f88e7facfe9abdbdf53c3a906b8b244a4d8
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2022-04-10 21:08:41 +0300

    create Enum project

---
+++ HomeworkEnum/src/by/epam/training/enums/Day.java
@@ -0,0 +1,12 @@
+package by.epam.training.enums;
+
+public enum Day {
+    MONDAY,
+    THUSDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+    
+}
---
+++ HomeworkEnum/src/by/epam/training/enums/EnumApp.java
@@ -0,0 +1,9 @@
+package by.epam.training.enums;
+
+public class EnumApp {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Done ...");
+    }
+}
